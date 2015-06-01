55165353a9d74d226b612a82aaf7248ef8f33f6a
sammcgrail <sammcgrail@gmail.com>
2015-06-01 11:28:31 -0400

    added cookie

--- server.rb
+++ server.rb
@@ -5,5 +5,15 @@ use Rack::Session::Cookie, {
 }
 
 get '/' do
-    erb :index
+    if session[:visit_count].nil?
+      visit_count = 1
+    else
+      # Everything in the session is stored as key-value strings. We need to
+      # convert back to an integer before we can use this value in our app.
+      visit_count = session[:visit_count].to_i
+    end
+    session[:visit_count] = visit_count + 1
+    "You've visited this page #{visit_count} time(s).\n"
+
+    erb :index, locals: {visit_count: visit_count}
 end
