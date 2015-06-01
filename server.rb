8814ff8228599fe0e46951a2adf6fd7748252bd2
sammcgrail <sammcgrail@gmail.com>
2015-06-01 12:00:50 -0400

    refactor lamonte old rps code

--- server.rb
+++ server.rb
@@ -5,77 +5,42 @@ use Rack::Session::Cookie, {
 }
 
 def choice(input)
-  times_lost = 0
-  times_won = 0
-  while times_won < 2 && times_lost < 2 do
-    puts("Player score: #{times_won}, Computer score: #{times_lost}")
-    puts("Choose rock (r), paper (p), or scissors (s): ")
+  p_score = 0
+  c_score = 0
 
-    if input == "r" || input == "p"|| input == "s"
-      puts "You've entered rock (r), paper (p), or scissors (s)"
-      if input == "r"
-        puts "Player chose rock."
-      elsif input == "p"
-        puts "Player chose paper."
-      elsif input == "s"
-        puts "Player chose scissors"
-      end
-      computer_roll = rand(3).to_i
-      # 0 = r
-      # 1 = p
-      # 2 = s
-        if computer_roll == 2
-          puts "Computer chose scissors."
-          if input == "p"
-            puts "Scissors beats paper, computer wins the round"
-            times_lost = times_lost + 1
-          elsif input == "s"
-            puts "Tie, choose again."
-          else input == "r"
-            puts "Rock beats scissors, player wins the round"
-            times_won = times_won + 1
+  while p_score < 3 || c_score < 3
+    puts "\nPlayer Score: #{p_score}, Computer Score: #{c_score}"
+    print 'Please choose rock (r), paper (p), or scissors (s):'
+    player = input
+    choices_p = { 'r' => 'rock', 'p' => 'paper', 's' => 'scissors'}
+    choices_c = { 0 => 'rock', 1 => 'paper', 2 => 'scissors'}
+    comp = rand(3)
+    if player == 'Rock' || player == 'Paper' || player == 'Scissors'
+        comp = choices_c[comp]
+            puts "Player chose #{player}."
+            puts "Computer chose #{comp}."
+            player_wins = "#{player} beats #{comp}, Player wins the round."
+            comp_wins = "#{comp} beats #{player}, Computer wins the round."
+            tie = "Tie, choose again."
+        if player == 'Rock' && comp == 'Scissors' || player == 'Paper' && comp == 'Rock' || player == 'Scissors' && comp == 'Paper'
+            puts player_wins
+            p_score += 1
+        elsif comp == 'Rock' && player == 'Scissors' || comp == 'Paper' && player == 'Rock' || comp == 'Scissors' && player == 'Paper'
+            puts comp_wins
+            c_score +=1
+        else
+            puts tie
         end
-        elsif computer_roll == 1
-          puts "Computer chose paper."
-          if input == "r"
-            puts "Paper beats rock, computer wins the round"
-            times_lost = times_lost + 1
-          elsif input == "p"
-            puts "Tie, choose again."
-          else input == "s"
-            puts "Scissors beats paper, player wins the round"
-            times_won = times_won + 1
-        end
-        else computer_roll == 0
-          puts "Computer chose rock."
-          if input == "s"
-            puts "Rock beats scissors, computer wins the round"
-            times_lost = times_lost + 1
-          elsif input == "r"
-            puts "Tie, choose again."
-          else input == "p"
-            puts "Paper beats rock, player wins the round"
-            times_won = times_won + 1
-        end
-      end
-      else
-      puts "Invalid entry, try again."
     end
-    if times_won == 2
-      puts
-      puts "Player wins!"
-      puts
-    end
-    if times_lost == 2
-      puts
-      puts "Computer wins!"
-      puts
+    if p_score == 3
+        puts "\nPlayer wins the game!"
+    else c_score == 3
+        puts "\nComputer wins the game!"
     end
   end
 end
 
-
-
+choice([params['value']])
 
 get '/' do
     if session[:visit_count].nil?
