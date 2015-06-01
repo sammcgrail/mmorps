84729279e4981a8dc093dc09d726773694fab221
sammcgrail <sammcgrail@gmail.com>
2015-06-01 17:04:25 -0400

    score counter running

--- .scratch.rb
+++ .scratch.rb
@@ -3,3 +3,7 @@ if p_score == 3
 else c_score == 3
     puts "\nComputer wins the game!"
 end
+
+from index erb locals
+player_score: session[:player_score],
+computer_score: session[:computer_score]
--- server.rb
+++ server.rb
@@ -5,6 +5,8 @@ use Rack::Session::Cookie, {
   secret: "keep_it_secret_keep_it_safe"
 }
 
+
+
 # def choice(params)
 #   p_score = 0
 #   c_score = 0
@@ -51,40 +53,63 @@ get '/' do
     visit_count = session[:visit_count].to_i
   end
   session[:visit_count] = visit_count + 1
-  "You've visited this page #{visit_count} time(s).\n"
   #COUNTER
 
-  session[:computer_choice] = ["rock","paper","scissors"].sample
-  computer_choice = session[:computer_choice]
-  player_choice = session[:player_choice]
+
+  if session[:computer_score].nil?
+    computer_score = 0
+  else
+    computer_score = session[:computer_score].to_i
+  end
+
+  if session[:player_score].nil?
+    player_score = 0
+  else
+    player_score = session[:player_score].to_i
+  end
+
   game_statement = session[:game_statement]
-  erb :index, locals: {visit_count: visit_count, player_choice: player_choice, computer_choice: computer_choice, game_statement: game_statement}
+
+  erb :index, locals: {
+                        visit_count: visit_count,
+                        player_choice: session[:player_choice],
+                        computer_choice: session[:computer_choice],
+                        game_statement: session[:game_statement],
+                        player_score: session[:player_score],
+                        computer_score: session[:computer_score]
+                      }
 end
 
 post '/'  do
+
   session[:player_choice] = params[:choice]
 
   computer_score = session[:computer_score]
   player_score = session[:player_score]
-  computer_choice = session[:computer_choice]
+
+  computer_choice = ["Rock","Paper","Scissors"].sample
+  session[:computer_choice] = computer_choice
+
   player_choice = session[:player_choice]
+
   player_wins = "#{player_choice} beats #{computer_choice}, Player wins the round."
-  comp_wins = "#{computer_choice} beats #{player_choice}, Computer wins the round."
+  computer_wins = "#{computer_choice} beats #{player_choice}, Computer wins the round."
   tie = "Tie, choose again."
 
-  game_statement = ""
+  session[:game_statement] = ""
 
   if player_choice == 'Rock' && computer_choice == 'Scissors' || player_choice == 'Paper' && computer_choice == 'Rock' || player_choice == 'Scissors' && computer_choice == 'Paper'
-      game_statement = player_wins
+      session[:game_statement] = player_wins
       player_score += 1
   elsif computer_choice == 'Rock' && player_choice == 'Scissors' || computer_choice == 'Paper' && player_choice == 'Rock' || computer_choice == 'Scissors' && player_choice == 'Paper'
-      game_statement = comp_wins
+      session[:game_statement] = computer_wins
       computer_score +=1
   else
-      game_statement = tie
+      session[:game_statement] = tie
   end
 
-  game_statement = session[:game_statement]
+  session[:player_score] = player_score
+  session[:computer_score] = computer_score
 
   redirect '/'
 end
