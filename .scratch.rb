fc447fb234005dd385b497b5ca86ca841a749523
sammcgrail <sammcgrail@gmail.com>
2015-06-01 13:49:40 -0400

    changed to buttons

---
+++ .scratch.rb
@@ -0,0 +1,5 @@
+if p_score == 3
+    puts "\nPlayer wins the game!"
+else c_score == 3
+    puts "\nComputer wins the game!"
+end
--- server.rb
+++ server.rb
@@ -1,46 +1,46 @@
 require "sinatra"
+require "pry"
 
 use Rack::Session::Cookie, {
   secret: "keep_it_secret_keep_it_safe"
 }
 
-def choice(input)
-  p_score = 0
-  c_score = 0
-
-  while p_score < 3 || c_score < 3
-    puts "\nPlayer Score: #{p_score}, Computer Score: #{c_score}"
-    print 'Please choose rock (r), paper (p), or scissors (s):'
-    player = input
-    choices_p = { 'r' => 'rock', 'p' => 'paper', 's' => 'scissors'}
-    choices_c = { 0 => 'rock', 1 => 'paper', 2 => 'scissors'}
-    comp = rand(3)
-    if player == 'Rock' || player == 'Paper' || player == 'Scissors'
-        comp = choices_c[comp]
-            puts "Player chose #{player}."
-            puts "Computer chose #{comp}."
-            player_wins = "#{player} beats #{comp}, Player wins the round."
-            comp_wins = "#{comp} beats #{player}, Computer wins the round."
-            tie = "Tie, choose again."
-        if player == 'Rock' && comp == 'Scissors' || player == 'Paper' && comp == 'Rock' || player == 'Scissors' && comp == 'Paper'
-            puts player_wins
-            p_score += 1
-        elsif comp == 'Rock' && player == 'Scissors' || comp == 'Paper' && player == 'Rock' || comp == 'Scissors' && player == 'Paper'
-            puts comp_wins
-            c_score +=1
-        else
-            puts tie
-        end
-    end
-    if p_score == 3
-        puts "\nPlayer wins the game!"
-    else c_score == 3
-        puts "\nComputer wins the game!"
-    end
-  end
-end
-
-choice([params['value']])
+# def choice(input)
+#   p_score = 0
+#   c_score = 0
+#
+#   while p_score < 3 || c_score < 3
+#     puts "\nPlayer Score: #{p_score}, Computer Score: #{c_score}"
+#     print 'Please choose rock (r), paper (p), or scissors (s):'
+#     player = input
+#     choices_c = { 0 => 'rock', 1 => 'paper', 2 => 'scissors'}
+#     comp = rand(3)
+#     if player == 'Rock' || player == 'Paper' || player == 'Scissors'
+#         comp = choices_c[comp]
+#             puts "Player chose #{player}."
+#             puts "Computer chose #{comp}."
+#             player_wins = "#{player} beats #{comp}, Player wins the round."
+#             comp_wins = "#{comp} beats #{player}, Computer wins the round."
+#             tie = "Tie, choose again."
+#         if player == 'Rock' && comp == 'Scissors' || player == 'Paper' && comp == 'Rock' || player == 'Scissors' && comp == 'Paper'
+#             puts player_wins
+#             p_score += 1
+#         elsif comp == 'Rock' && player == 'Scissors' || comp == 'Paper' && player == 'Rock' || comp == 'Scissors' && player == 'Paper'
+#             puts comp_wins
+#             c_score +=1
+#         else
+#             puts tie
+#         end
+#     end
+#     if p_score == 3
+#         puts "\nPlayer wins the game!"
+#     else c_score == 3
+#         puts "\nComputer wins the game!"
+#     end
+#   end
+# end
+#
+# choice('Rock')
 
 get '/' do
     if session[:visit_count].nil?
@@ -53,5 +53,16 @@ get '/' do
     session[:visit_count] = visit_count + 1
     "You've visited this page #{visit_count} time(s).\n"
 
-    erb :index, locals: {visit_count: visit_count}
+
+    session[:player_choice] = params[:choice]
+    player_choice = session[:player_choice]
+
+    session[:computer_choice] = rand(3)
+    computer_choice = session[:computer_choice]
+
+    erb :index, locals: {visit_count: visit_count, player_choice: player_choice, computer_choice: computer_choice}
+end
+
+post '/'  do
+    redirect '/'
 end
